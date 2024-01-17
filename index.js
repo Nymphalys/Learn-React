860bd19ce1b96648d224e5aaabd27a73c6805ffa
Nymphalys <kumariladli288@gmail.com>
2024-01-17 18:34:53 +0530

    tried some DOM rendenring

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-ReactDOM.render(<h1>Hello Cutie</h1>, document.getElementById("root"))
+ReactDOM.render(
+    <div>
+      <h1>Hello Cutie</h1>
+      <br/>
+      <p>Hows mmy Sugarplum doin?</p>
+      <ul>
+          <li>Ice Cream</li>
+          <li>Coffee Date</li>
+       </ul>
+    </div>
+, document.getElementById("root"))
